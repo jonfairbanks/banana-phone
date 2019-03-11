bac7c11f433753d3132986c6942db3c93b71bd2b
Jon Fairbanks <fairplay89@gmail.com>
2019-03-11 01:53:58 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const config = require('./config/config.json')
+const twilio = require('twilio')
+const client = new twilio(config.accountSid, config.authToken)
+
+const to = '+1' + process.argv[2];
+
+console.log('Calling ' + to + '...')
+
+var promise = client.calls.create({
+    url: config.xmlUrl,
+    to: to,
+    from: config.from,
+})
+
+promise.then(call => {
+    console.log('Call completed: ' + call.sid)
+}), (error) => {
+    console.error(error)
+};
