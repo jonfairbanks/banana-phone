ecf2c358aea9b4013cb03de0655a60d35633b0ba
Jon Fairbanks <fairplay89@gmail.com>
2019-03-11 17:38:21 -0700

    Remove Hardcoded Prefix

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const config = require('./config/config.json')
 const twilio = require('twilio')
 const client = new twilio(config.accountSid, config.authToken)
 
-const to = '+1' + process.argv[2]
-
-console.log('Calling ' + to + '...')
+const to = process.argv[2]
 
 var promise = client.calls.create({
     url: config.xmlUrl,
@@ -14,7 +12,7 @@ var promise = client.calls.create({
 })
 
 promise.then(call => {
-    console.log('Call completed: ' + call.sid)
+    console.log('Call to ' + to + ' completed: ' + call.sid)
 }), (error) => {
     console.error(error)
 }
